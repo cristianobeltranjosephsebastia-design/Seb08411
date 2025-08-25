9aa5f2e47bb1e2bf0a8cbf1a5b4f1273f599a16a
Sebastian <cristianobeltranjosephsebastia@gmail.com>
2025-08-24 21:22:45 -0500

    Modulo 6

---
+++ Modulo6/CicloForContinue.py
@@ -0,0 +1,17 @@
+for I in range (5):
+    print("Esto continua") 
+    print(I)
+
+print("Tabla de multiplicar del 3")
+
+for M in range (11):
+ print(M*3)
+
+print("Tabla de multiplicar")
+
+Numero = int(input("Ingresar un numero"))
+for N in range (11):
+ print(f"{Numero} x {N} = {Numero*N}")   
+
+print("¡Gracias!")
+ 
